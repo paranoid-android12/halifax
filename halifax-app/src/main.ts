bc954021774520f3df2b9cfe9e51d9a9df422ca9
Erlein Nicole <erleinureta@gmail.com>
2024-07-17 00:48:39 +0800

    no login rendering

--- halifax-app/src/main.ts
+++ halifax-app/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
 import { enableProdMode, importProvidersFrom } from "@angular/core";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [provideHttpClient()],
 }).catch((err) => console.error(err));
